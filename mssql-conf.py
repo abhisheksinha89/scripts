3db17809a62de4a4c7c60b6b0655de2f821ed813
abhisabh <abhisabh@microsoft.com>
2016-08-12 13:56:44 -0700

    Parse INI file script

---
+++ mssql-conf.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+import sys
+from ConfigParser import ConfigParser
+
+_defaultConfigFile="test.ini"
+
+#Get the command line parameters
+if len(sys.argv) == 2:
+	_defaultConfigFile=sys.argv[1]
+elif len(sys.argv) > 2:
+	print("Usage: mssql-conf <fileName>\n<fileName> (optional) is the path to the config file, if not specified the default path is used")
+
+# instantiate
+config = ConfigParser()
+
+# parse existing file
+config.read(_defaultConfigFile)
+
+# read values from a section
+string_val = config.get('section_a', 'string_val')
+bool_val = config.getboolean('section_a', 'bool_val')
+int_val = config.getint('section_a', 'int_val')
+float_val = config.getfloat('section_a', 'pi_val')
+
+print(string_val)
+print(bool_val)
+print(int_val)
+print(float_val)
