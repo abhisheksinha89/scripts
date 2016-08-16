92c5a12c3f3891943389020cbb1929780042b3a1
abhisabh <abhisabh@microsoft.com>
2016-08-16 15:40:12 -0700

    Script for Initial TempDb File Count intitial checkin

---
+++ InitialTempDbFileCount.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#Get the number of Initial Sql Temp Db files to be created
+re='^[0-9]+$'
+numberOfFiles=1
+matchGetOptsArgs=0
+zero=0
+
+usage() { echo "Usage: $0 [-n <number of TempDb files to create>]"; exit 1; }
+
+
+while getopts ":h?n:" opt; do
+	case "${opt}" in
+	  n)
+  	  echo "Number of Initial Sql TempDb Files to be created: $OPTARG" >&2
+      numberOfFiles=$OPTARG
+      matchGetOptsArgs+=1
+      ;;
+    h|\?)
+      usage
+      ;;
+    *)
+      usage    
+  esac
+done	
+
+if [ $matchGetOptsArgs -eq 0 ]; 
+  then
+    usage
+fi
+
